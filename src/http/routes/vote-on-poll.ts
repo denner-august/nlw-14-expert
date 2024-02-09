f2da3a1c0e3207fda997797a095d4e34dc5eeca0
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-02-09 19:43:26 -0300

    Update vote-on-poll.ts

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,6 +1,9 @@
 import z from "zod";
 import { FastifyInstance } from "fastify";
 import { randomUUID } from "node:crypto";
+import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
+import { voting } from "../../utils/voting-pub-sub";
 
 export async function VoteOnPoll(app: FastifyInstance) {
   app.post("/polls/:pollId/votes", async (req, res) => {
@@ -15,15 +18,69 @@ export async function VoteOnPoll(app: FastifyInstance) {
     const { pollId } = voteOnParams.parse(req.params);
     const { pollOptionId } = voteOnBody.parse(req.body);
 
-    const sessionId = randomUUID();
+    let { sessionID } = req.cookies;
 
-    res.setCookie("sessionId", sessionId, {
-      path: "/",
-      maxAge: 60 * 60 * 24 * 30, // 30 days
-      signed: true,
-      httpOnly: true,
+    if (sessionID) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionID_pollId: {
+            sessionID,
+            pollId,
+          },
+        },
+      });
+
+      if (
+        userPreviousVoteOnPoll &&
+        userPreviousVoteOnPoll.pollOptionId !== pollOptionId
+      ) {
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id,
+          },
+        });
+
+        const votes = await redis.zincrby(
+          pollId,
+          -1,
+          userPreviousVoteOnPoll.pollOptionId
+        );
+
+        voting.publish(pollId, {
+          pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+          votes: Number(votes),
+        });
+      } else if (userPreviousVoteOnPoll) {
+        return res.status(400).send({ message: "você já votou nessa enquete" });
+      }
+    }
+
+    if (!sessionID) {
+      sessionID = randomUUID();
+
+      res.setCookie("sessionID", sessionID, {
+        path: "/",
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        signed: true,
+        httpOnly: true,
+      });
+    }
+
+    await prisma.vote.create({
+      data: {
+        sessionID,
+        pollOptionId,
+        pollId,
+      },
     });
 
-    return res.status(201).send();
+    const votes = await redis.zincrby(pollId, 1, pollOptionId);
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes),
+    });
+
+    return res.status(201).send({ sessionID });
   });
 }
