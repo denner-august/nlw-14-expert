b4048c52a8390e36c47422a6c88885f78894aae5
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-02-09 19:43:23 -0300

    Update get-poll.ts

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,3 +1,4 @@
+import { redis } from "./../../lib/redis";
 import z from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
@@ -25,6 +26,35 @@ export async function getPoll(app: FastifyInstance) {
       },
     });
 
-    return res.send({ poll });
+    if (!poll) {
+      return res.status(400).send({ message: "não encontrado" });
+    }
+
+    const result = await redis.zrange(pollid, 0, -1, "WITHSCORES");
+
+    const votes = result.reduce((prev, next, index) => {
+      if (index % 2 === 0) {
+        const score = result[index + 1];
+        Object.assign(prev, { [next]: Number(score) });
+      }
+
+      return prev;
+    }, {} as Record<string, number>);
+
+    console.log(votes);
+
+    return res.send({
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map((option) => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: option.id in votes ? votes[option.id] : 0,
+          };
+        }),
+      },
+    });
   });
 }
